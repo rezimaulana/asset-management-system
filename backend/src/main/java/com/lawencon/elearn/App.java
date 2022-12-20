188430753d6a22517bcb6c1532f661b4eaf736bc
rezimaulana <rsazrm@gmail.com>
2022-12-21 03:37:41 +0700

    Updated SpringServlet

--- backend/src/main/java/com/lawencon/elearn/App.java
+++ backend/src/main/java/com/lawencon/elearn/App.java
@@ -2,9 +2,10 @@ package com.lawencon.elearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
